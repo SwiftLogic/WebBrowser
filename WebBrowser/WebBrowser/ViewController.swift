8924dc7f473b63c98993d8b76c2c8dcd6f5edb82
Samuel Uyigue <Samuel.uyigue@gmail.com>
2022-09-09 11:02:51 -0500

    Created xcode project

---
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebBrowser
+//
+//  Created by Osaretin Uyigue on 9/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
