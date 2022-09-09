0e8abf93722cde76886611ab985e3e5210ce5b0d
Samuel Uyigue <Samuel.uyigue@gmail.com>
2022-09-09 17:16:24 -0500

    Added search functionality

--- WebBrowser/WebBrowser/ViewController.swift
+++ WebBrowser/WebBrowser/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import WebKit
 class ViewController: UIViewController {
-
+    
     
     //MARK: - View's lifeCycle
     override func viewDidLoad() {
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         view.backgroundColor = .zircon
         configureNavBar()
         setUpViews()
+        load(urlString: "http://www.google.com")
     }
     
     
@@ -24,12 +25,15 @@ class ViewController: UIViewController {
     fileprivate lazy var searchBar: UISearchBar = {
         let sb = UISearchBar()
         sb.placeholder = "Search or enter website"
+        sb.delegate = self
         return sb
     }()
     
     
     fileprivate lazy var webView: WKWebView = {
         let webview = WKWebView()
+        webview.navigationDelegate = self
+        webview.allowsBackForwardNavigationGestures = true
         return webview
     }()
     
@@ -38,13 +42,7 @@ class ViewController: UIViewController {
     fileprivate func setUpViews() {
         view.addSubview(webView)
         webView.frame = view.bounds
-
-        //"https://samisays11.github.io/"
-        
-        guard let url = URL(string: "http://www.google.com") else {return}
-        webView.load(URLRequest(url: url))
     }
-    
     
     fileprivate func configureNavBar() {
         navigationItem.titleView = searchBar
@@ -53,23 +51,21 @@ class ViewController: UIViewController {
         setUpBottomToolbar()
     }
     
-    
     fileprivate func setUpBottomToolbar() {
         let forward = UIBarButtonItem(barButtonSystemItem: .fastForward, target: self, action: #selector(didTapGoForward))
         let backward = UIBarButtonItem(barButtonSystemItem: .rewind, target: self, action: #selector(didTapGoBackward))
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(didTapRefresh))
         let close = UIBarButtonItem(barButtonSystemItem: .stop, target: self, action: #selector(didTapStopLoading))
-
+        
         let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
         toolbarItems = [backward, forward, spacer, refresh, close]
         navigationController?.setToolbarHidden(false, animated: false)
     }
     
-    
     fileprivate func configureNavBarAppearance() {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "🌎 WebBrowser"
-
+        
         let appearance = UINavigationBarAppearance()
         appearance.backgroundColor = .white
         appearance.titleTextAttributes = [.foregroundColor: UIColor.black]
@@ -79,9 +75,7 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.standardAppearance = appearance
         navigationController?.navigationBar.compactAppearance = appearance
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
-        
     }
-
     
     fileprivate func configureToolBarAppearance() {
         let toolBarAppearance = UIToolbarAppearance()
@@ -90,6 +84,11 @@ class ViewController: UIViewController {
         navigationController?.toolbar.standardAppearance = toolBarAppearance
         navigationController?.toolbar.compactAppearance = toolBarAppearance
         navigationController?.toolbar.scrollEdgeAppearance = toolBarAppearance
+    }
+    
+    fileprivate func load(urlString: String) {
+        guard let url = URL(string: urlString) else {return}
+        webView.load(URLRequest(url: url))
     }
     
 }
@@ -122,8 +121,35 @@ extension ViewController {
 }
 
 
+//MARK: - WKNavigationDelegate
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        //
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        //
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+    }
+}
+
+
+
+//MARK: - UISearchBarDelegate
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        let urlString = "http://\(searchBar.text ?? "")"
+        load(urlString: urlString)
+    }
+}
+
+
 extension UIColor {
-  static let zircon = UIColor(named: "Zircon")!
+    static let zircon = UIColor(named: "Zircon")!
 }
 
 
